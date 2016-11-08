b6a99bcabc2ae13af0464079da1323e567c3cf8b
TranToan2910 <shiro2910@gmail.com>
2016-11-08 21:57:04 +0700

    Update skeleton CPU OperatingSystem

--- src/com/tsdv/javatraining/ossimulator/model/Instruction.java
+++ src/com/tsdv/javatraining/ossimulator/model/Instruction.java
@@ -14,7 +14,7 @@ import java.util.List;
  */
 public class Instruction {
 
-    private int optCode;
+    private InstructionInfo optCode;
     private List<Integer> operandList;
 
     public Instruction() {
