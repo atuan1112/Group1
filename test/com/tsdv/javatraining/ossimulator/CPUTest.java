77848d8823e8b1fe6884c53ec9925bf68e75a8e5
TranToan2910 <shiro2910@gmail.com>
2016-11-08 22:12:09 +0700

    Update skeleton CPU OperatingSystem

---
+++ test/com/tsdv/javatraining/ossimulator/CPUTest.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tsdv.javatraining.ossimulator;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Rintaro
+ */
+public class CPUTest {
+    
+    public CPUTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of setTimer method, of class CPU.
+     */
+    @Test
+    public void testSetTimer() {
+        Memory memory = new Memory(2000);
+        try {
+            CPU cpu = new CPU(memory);
+            cpu.setTimer(0, 500);
+        } catch (UnsupportedOperationException e) {
+            fail("Not implement");
+        }
+    }
+
+    /**
+     * Test of connectPeripheral method, of class CPU.
+     */
+    @Test
+    public void testConnectPeripheral() {
+        Memory memory = new Memory(2000);
+        TextDisplayer textDisplayer = new TextDisplayer(1);
+        try {
+            CPU cpu = new CPU(memory);
+            cpu.connectPeripheral(0, textDisplayer);
+        } catch (UnsupportedOperationException e) {
+            fail("Not implement");
+        }
+    }
+
+    /**
+     * Test of start method, of class CPU.
+     */
+    @Test
+    public void testStart() {
+        Memory memory = new Memory(2000);
+        try {
+            CPU cpu = new CPU(memory);
+            cpu.start();
+        } catch (UnsupportedOperationException e) {
+            fail("Not implement");
+        }
+    }
+    
+}
